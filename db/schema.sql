0e8e3ffe7f4bb33170cdb64e48421d76b8731910
Jessmica11 <116199836+Jessmica11@users.noreply.github.com>
2023-12-10 17:35:48 -0500

    reorganized tables for clarity

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,23 @@
-/* start with blank slate for employee db */
+/* start with blank slate for employees db */
 DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
+
+/* department table */
+CREATE TABLE department (
+    id INT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+)
+
+/* role table */
+CREATE TABLE role (
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id)
+)
 
 /* employee table */
 CREATE TABLE employee (
@@ -8,19 +25,6 @@ CREATE TABLE employee (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
-  manager_id INT 
+  manager_id INT,
+  FOREIGN KEY (role_id)
 );
-
-/* department table */
-CREATE TABLE department (
-    id INT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
-)
-
-/* role table */
-CREATE TABLE role (
-    id INT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL 
-    department_id INT NOT NULL
-)
