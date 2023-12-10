4b6a8c7837ab0695eb82d12fc4235d21f681c3da
Jessmica11 <116199836+Jessmica11@users.noreply.github.com>
2023-12-10 17:52:49 -0500

    expanding inquirer prompts, functions, for input

--- server.js
+++ server.js
@@ -1,12 +1,62 @@
 // require mySQL2, express, inquirer v 8.2.4
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const express = require('express');
 
+const PORT = process.env.PORT || 3002;
+const app = express();
 
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // database const with login info for mySQL
-
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: 'JessMySQL23',
+    database: 'employees_db'
+  },
+  console.log(`Connected to the employees_db database.`)
+);
 
 // query database
 
-// 
+// inquirer questions at start
+// Function to start the application
+function startApp() {
+  inquirer
+    .prompt({
+      name: 'action',
+      type: 'list',
+      message: 'What would you like to do?',
+      // choices provided in Acceptance Criteria
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+        'Exit',
+      ],
+    })
+
+
+// functions for each event to happen 
+// depending on user choise
+
+
 
 // Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
