ff19fe8c4bf7d5d87fce33db9b3a5ed41ddf3440
Jessmica11 <116199836+Jessmica11@users.noreply.github.com>
2023-12-10 19:13:54 -0500

    expanded all functions for each user choice

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const express = require('express');
 
+// choose a PORT to use for running app
 const PORT = process.env.PORT || 3002;
 const app = express();
 
@@ -11,22 +12,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // database const with login info for mySQL
-const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    // MySQL username,
+const db = mysql.createConnection({
+  host: 'localhost',
+// MySQL username,
     user: 'root',
-    // MySQL password
-    password: 'JessMySQL23',
-    database: 'employees_db'
-  },
-  console.log(`Connected to the employees_db database.`)
-);
+// MySQL password
+  password: '',
+  database: 'employees_db',
+});
 
-// query database
+// let the user know if there's an error connecting
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  // tell the user they've connected to the db
+  console.log('Connected to the employees_db database.');
+  startApp();
+});
 
 // inquirer questions at start
-// Function to start the application
+// function to start the application
 function startApp() {
   inquirer
     .prompt({
@@ -49,43 +56,36 @@ function startApp() {
     .then((answer) => {
       switch (answer.action) {
         case 'View all departments':
-    // start viewDepartments() when this choice is made
+        // start viewDepartments() when this choice is made
           viewDepartments();
           break;
-
         case 'View all roles':
-    // start viewRoles() when this choice is made
+        // start viewRoles() when this choice is made
           viewRoles();
           break;
-
         case 'View all employees':
-    // start viewEmployees() when this choice is made
+        // start viewEmployees() when this choice is made
           viewEmployees();
           break;
-
         case 'Add a department':
-    // start addDepartment() when this choice is made
+        // start addDepartment() when this choice is made
           addDepartment();
           break;
-
         case 'Add a role':
-    // start addRole() when this choice is made
+            // start addRole() when this choice is made
           addRole();
           break;
-
         case 'Add an employee':
-    // start addEmployee() when this choice is made
+        // start addEmployee() when this choice is made
           addEmployee();
           break;
-
         case 'Update an employee role':
-    // start updateEmployeeRole() when this choice is made
+        // start updateEmployeeRole() when this choice is made
           updateEmployeeRole();
           break;
-
         case 'Exit':
-    // Exit = end the program
-          connection.end();
+        // end the program when exit is chosen
+          db.end();
           break;
       }
     });
@@ -94,14 +94,206 @@ function startApp() {
 // functions for each event to happen, 
 // depending on user choice
 
+function viewDepartments() {
+  const query = 'SELECT * FROM department';
+  db.promise()
+    .query(query)
+    .then(([rows]) => {
+      console.table(rows);
+      startApp();
+    })
+    .catch((err) => {
+      console.error('Error:', err);
+      startApp();
+    });
+}
 
+function viewRoles() {
+  const query =
+    'SELECT role.title, role.salary, department.name AS department FROM role JOIN department ON role.department_id = department.id';
+  db.promise()
+    .query(query)
+    .then(([rows]) => {
+      console.table(rows);
+      startApp();
+    })
+    .catch((err) => {
+      console.error('Error:', err);
+      startApp();
+    });
+}
 
+function viewEmployees() {
+  const query =
+    'SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS department, CONCAT(manager.first_name, " ", manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id';
+  db.promise()
+    .query(query)
+    .then(([rows]) => {
+      console.table(rows);
+      startApp();
+    })
+    .catch((err) => {
+      console.error('Error:', err);
+      startApp();
+    });
+}
+
+// functions for adding items, 
+// depending on user choice
+
+function addDepartment() {
+  inquirer
+    .prompt({
+      name: 'name',
+      type: 'input',
+      message: 'Enter the name of the department:',
+    })
+    .then((answer) => {
+      const query = 'INSERT INTO department SET ?';
+      db.promise()
+        .query(query, { name: answer.name })
+        .then(() => {
+          console.log('Department added successfully!');
+          startApp();
+        })
+        .catch((err) => {
+          console.error('Error:', err);
+          startApp();
+        });
+    });
+}
+
+function addRole() {
+  inquirer
+    .prompt([
+      {
+        name: 'title',
+        type: 'input',
+        message: 'Enter the title of the role:',
+      },
+      {
+        name: 'salary',
+        type: 'input',
+        message: 'Enter the salary for the role:',
+      },
+      {
+        name: 'department_id',
+        type: 'input',
+        message: 'Enter the department ID for the role:',
+      },
+    ])
+    .then((answer) => {
+      const query = 'INSERT INTO role SET ?';
+      db.promise()
+        .query(query, answer)
+        .then(() => {
+          console.log('Role added successfully!');
+          startApp();
+        })
+        .catch((err) => {
+          console.error('Error:', err);
+          startApp();
+        });
+    });
+}
+
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        name: 'first_name',
+        type: 'input',
+        message: "Enter the employee's first name:",
+      },
+      {
+        name: 'last_name',
+        type: 'input',
+        message: "Enter the employee's last name:",
+      },
+      {
+        name: 'role_id',
+        type: 'input',
+        message: "Enter the employee's role ID:",
+      },
+      {
+        name: 'manager_id',
+        type: 'input',
+        message: "Enter the employee's manager ID:",
+      },
+    ])
+    .then((answer) => {
+      const query = 'INSERT INTO employee SET ?';
+      db.promise()
+        .query(query, answer)
+        .then(() => {
+          console.log('Employee added successfully!');
+          startApp();
+        })
+        .catch((err) => {
+          console.error('Error:', err);
+          startApp();
+        });
+    });
+}
+
+// functions for updating items, 
+// depending on user choice
+
+function updateEmployeeRole() {
+  db.promise().query('SELECT * FROM employee')
+    .then(([employees]) => {
+      inquirer
+        .prompt({
+          name: 'employeeId',
+          type: 'list',
+          message: 'Select the employee you want to update:',
+          choices: employees.map((employee) => ({
+            name: `${employee.first_name} ${employee.last_name}`,
+            value: employee.id,
+          })),
+        })
+        .then((employeeAnswer) => {
+          db.promise().query('SELECT * FROM role')
+            .then(([roles]) => {
+              inquirer
+                .prompt({
+                  name: 'roleId',
+                  type: 'list',
+                  message: 'Select the new role for the employee:',
+                  choices: roles.map((role) => ({
+                    name: role.title,
+                    value: role.id,
+                  })),
+                })
+                .then((roleAnswer) => {
+                  db.promise().query(
+                    'UPDATE employee SET role_id = ? WHERE id = ?',
+                    [roleAnswer.roleId, employeeAnswer.employeeId]
+                  )
+                    .then(() => {
+                      console.log('Employee role updated successfully!');
+                      startApp();
+                    })
+                    .catch((err) => {
+                      console.error('Error:', err);
+                      startApp();
+                    });
+                });
+            });
+        });
+    })
+    .catch((err) => {
+      console.error('Error:', err);
+      startApp();
+    });
+}
 
 // Default response for any other request (Not Found)
+
 app.use((req, res) => {
   res.status(404).end();
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
