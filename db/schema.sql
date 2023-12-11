b444b965622f9e1f743c4086a25062620ea775c4
Jessmica11 <116199836+Jessmica11@users.noreply.github.com>
2023-12-10 19:18:37 -0500

    adding all updates to repo

--- db/schema.sql
+++ db/schema.sql
@@ -1,30 +1,31 @@
-/* start with blank slate for employees db */
+/* start with a blank slate for employees db */
 DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 
+/* use the created database */
+USE employees_db;
+
 /* department table */
 CREATE TABLE department (
-    id INT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
-)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
 
 /* role table */
 CREATE TABLE role (
-    id INT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (department_id)
-)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
 
 /* employee table */
 CREATE TABLE employee (
-  id INT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
   manager_id INT,
-  FOREIGN KEY (role_id)
+  FOREIGN KEY (role_id) REFERENCES role(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,19 @@
-/* for adding into employee table */
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES
-    (1, 'Jane', 'Doe', 1, 1),
-    (2,'Jessica', 'Scheck', 2, NULL);
+/*  found this online as a way to clear the tables for each test I would run */
+TRUNCATE TABLE department;
+TRUNCATE TABLE role;
+TRUNCATE TABLE employee;
 
 /* for adding into department table */
 INSERT INTO department (id, name) VALUES
-    (1,'Legal'),
-    (2,'Engineering');  
+    (1, 'Legal'),
+    (2, 'Engineering');
 
 /* for adding into role table */
 INSERT INTO role (id, title, salary, department_id) VALUES
-    (1,'Attorney', 100000.00, 1),
-    (2, 'Software Engineer', 120000.00, 2);  
+    (1, 'Attorney', 100000.00, 1),
+    (2, 'Software Engineer', 120000.00, 2);
+
+/* for adding into employee table */
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES
+    (1, 'Jane', 'Doe', 1, 1),
+    (2, 'Jessica', 'Scheck', 2, NULL);
